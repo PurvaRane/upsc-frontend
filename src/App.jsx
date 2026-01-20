f840b4552df2948ae1a1b6466aaf55e1192bdcdd
sachinmandate <sachinpatil@mandates.in>
2026-01-20 19:21:25 +0530

    Initial React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-800">
+        <h1 className="text-4xl font-bold text-white">
+          START YOUR PROJECT
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
